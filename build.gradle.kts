116fa3ebe1c2a73db555a4f4fbc44f4149714240
jssdvv <jssdvv@gmail.com>
2024-01-24 15:33:44 -0500

    Added Serialization and SQLDelight Plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.compose) apply false
     alias(libs.plugins.jetbrains.kotlin.multiplatform) apply false
+    alias(libs.plugins.jetbrains.kotlin.plugin.serialization) apply false
+    alias(libs.plugins.cashapp.sqldelight) apply false
 }
