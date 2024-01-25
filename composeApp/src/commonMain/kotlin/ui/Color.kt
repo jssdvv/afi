2b84e8b590a7d2dcd20597be679981a030429892
jssdvv <jssdvv@gmail.com>
2024-01-24 21:16:35 -0500

    Added base files to setup material theme

---
+++ composeApp/src/commonMain/kotlin/ui/Color.kt
@@ -0,0 +1,94 @@
+package ui
+
+import androidx.compose.ui.graphics.Color
+
+//Institutional UIS Logo Colors
+val GreenLogo = Color(0xFF67B93E)
+val BlackLogo = Color(0xFF1F1A17)
+val GrayLogo = Color(0xFF969594)
+
+//Faculties Background/Typography Colors
+val BlueHealth = Color(0xFF0071B6)
+val BlueHumanSciences = Color(0xFF00A4Bf)
+val OrangeSciences = Color(0xFFC99500)
+val OrangePhysicalMechanical = Color(0xFFE67015)
+val RedPhysicalChemical = Color(0xFFdA261D)
+val PurpleDistanceEducation = Color(0xFF7A1A66)
+
+//Primary key colors
+val Green0 = Color.Black
+val Green10 = Color(0xFF15260D)
+val Green20 = Color(0xFF2A4D19)
+val Green30 = Color(0xFF407326)
+val Green40 = Color(0xFF559933)
+val Green50 = Color(0xFF6ABF40)
+val Green60 = Color(0xFF88CC66)
+val Green70 = Color(0xFFA6D98C)
+val Green80 = Color(0xFFC3E6B3)
+val Green90 = Color(0xFFE1F2D9)
+val Green100 = Color.White
+
+//Secondary key colors, 20% saturation of GreenLogo color
+val GreenGray0 = Color.Black
+val GreenGray10 = Color(0xFF181f14)
+val GreenGray20 = Color(0xFF303d29)
+val GreenGray30 = Color(0xFF475c3d)
+val GreenGray40 = Color(0xFF5f7a52)
+val GreenGray50 = Color(0xFF779966)
+val GreenGray60 = Color(0xFF92ad85)
+val GreenGray70 = Color(0xFFadc2a3)
+val GreenGray80 = Color(0xFFc9d6c2)
+val GreenGray90 = Color(0xFFe4ebe0)
+val GreenGray100 = Color.White
+
+//Tertiary key colors, 180 Hue blue color in the same chromatic circle of GreenLogo color
+val Blue0 = Color.Black
+val Blue10 = Color(0xFF0d2626)
+val Blue20 = Color(0xFF194d4d)
+val Blue30 = Color(0xFF267373)
+val Blue40 = Color(0xFF339999)
+val Blue50 = Color(0xFF40bfbf)
+val Blue60 = Color(0xFF66cccc)
+val Blue70 = Color(0xFF8cd9d9)
+val Blue80 = Color(0xFFb3e6e6)
+val Blue90 = Color(0xFFd9f2f2)
+val Blue100 = Color.White
+
+//Neutral key colors
+val Gray0 = Color.Black
+val Gray10 = Color(0xFF1A1A1A)
+val Gray20 = Color(0xFF333333)
+val Gray30 = Color(0xFF4D4D4D)
+val Gray40 = Color(0xFF666666)
+val Gray50 = Color(0xFF808080)
+val Gray60 = Color(0xFF999999)
+val Gray70 = Color(0xFFB3B3B3)
+val Gray80 = Color(0xFFCCCCCC)
+val Gray90 = Color(0xFFE6E6E6)
+val Gray100 = Color.White
+
+//Neutral variant key colors
+val DarkGreenGray0 = Color.Black
+val DarkGreenGray10 = Color(0xFF191c17)
+val DarkGreenGray20 = Color(0xFF32382e)
+val DarkGreenGray30 = Color(0xFF4b5445)
+val DarkGreenGray40 = Color(0xFF64705c)
+val DarkGreenGray50 = Color(0xFF7d8c73)
+val DarkGreenGray60 = Color(0xFF97a38f)
+val DarkGreenGray70 = Color(0xFFb1baab)
+val DarkGreenGray80 = Color(0xFFcbd1c7)
+val DarkGreenGray90 = Color(0xFFe5e8e3)
+val DarkGreenGray100 = Color.White
+
+//Error key colors
+val Red0 = Color.Black
+val Red10 = Color(0xFF330500)
+val Red20 = Color(0xFF660a00)
+val Red30 = Color(0xFF990f00)
+val Red40 = Color(0xFFcc1400)
+val Red50 = Color(0xFFff1a00)
+val Red60 = Color(0xFFff4733)
+val Red70 = Color(0xFFff7566)
+val Red80 = Color(0xFFffa399)
+val Red90 = Color(0xFFffd1cc)
+val Red100 = Color.White
---
+++ composeApp/src/commonMain/kotlin/ui/Shape.kt
@@ -0,0 +1,14 @@
+package ui
+
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Shapes
+
+class Shapes {
+    val roundedShapes = Shapes(
+        extraSmall = ShapeDefaults.ExtraSmall,
+        small = ShapeDefaults.Small,
+        medium = ShapeDefaults.Medium,
+        large = ShapeDefaults.Large,
+        extraLarge = ShapeDefaults.ExtraLarge,
+    )
+}
