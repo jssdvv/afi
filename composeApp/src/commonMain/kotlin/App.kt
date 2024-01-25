edb67a57e8c60957f9d666f1584bd1eb1c87a496
jssdvv <jssdvv@gmail.com>
2024-01-24 21:23:29 -0500

    Added material theme to shared Main function: App()

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,6 +1,18 @@
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import ui.Theme
 
 @Composable
 fun App() {
+    Theme {
+        Surface (
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
 
+        }
+    }
 }
