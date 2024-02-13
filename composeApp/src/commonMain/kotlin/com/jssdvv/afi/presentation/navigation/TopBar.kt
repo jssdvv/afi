783bea3d75984914476f25d848966e4e6432368a
jssdvv <jssdvv@gmail.com>
2024-02-13 17:05:08 -0500

    Created basic top app bar with single title

---
+++ composeApp/src/commonMain/kotlin/com/jssdvv/afi/presentation/navigation/TopBar.kt
@@ -0,0 +1,16 @@
+package com.jssdvv.afi.presentation.navigation
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import com.jssdvv.afi.MR
+import dev.icerock.moko.resources.compose.stringResource
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = { Text(stringResource(MR.strings.app_title)) }
+    )
+}
