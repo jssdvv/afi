b5621a6e5aa33e131945eda223dc22090afafd6f
jssdvv <jssdvv@gmail.com>
2024-02-12 23:48:41 -0500

    Changed screens to tab interfaces

---
+++ composeApp/src/commonMain/kotlin/com/jssdvv/afi/presentation/features/directory/DirectoryTab.kt
@@ -0,0 +1,29 @@
+package com.jssdvv.afi.presentation.features.directory
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabOptions
+
+class DirectoryTab : Tab {
+    override val options: TabOptions
+        @Composable
+        get() {
+            val title = "Directory"
+            val icon = null
+
+            return remember {
+                TabOptions(
+                    index = 0u,
+                    title = title,
+                    icon = null
+                )
+            }
+        }
+
+    @Composable
+    override fun Content() {
+        Text("Pantalla directorio")
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/jssdvv/afi/presentation/features/scanner/ScannerTab.kt
@@ -0,0 +1,26 @@
+package com.jssdvv.afi.presentation.features.scanner
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabOptions
+
+class ScannerTab : Tab {
+    override val options: TabOptions
+        @Composable
+        get() {
+            return remember {
+                TabOptions(
+                    index = 2u,
+                    title = "",
+                    icon = null
+                )
+            }
+        }
+
+    @Composable
+    override fun Content() {
+        Text("Pantalla escaner")
+    }
+}
