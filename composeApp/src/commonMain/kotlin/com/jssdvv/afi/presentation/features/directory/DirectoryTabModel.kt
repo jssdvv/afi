0afd09e173ab962d98d90e280628ca536bf1869a
jssdvv <jssdvv@gmail.com>
2024-02-13 22:57:36 -0500

    Added screen models (view models) to tabs destinations

---
+++ composeApp/src/commonMain/kotlin/com/jssdvv/afi/presentation/features/directory/DirectoryTabModel.kt
@@ -0,0 +1,7 @@
+package com.jssdvv.afi.presentation.features.directory
+
+import cafe.adriel.voyager.core.model.ScreenModel
+
+class DirectoryTabModel : ScreenModel {
+
+}
---
+++ composeApp/src/commonMain/kotlin/com/jssdvv/afi/presentation/features/scanner/ScannerTabModel.kt
@@ -0,0 +1,7 @@
+package com.jssdvv.afi.presentation.features.scanner
+
+import cafe.adriel.voyager.core.model.ScreenModel
+
+class ScannerTabModel : ScreenModel {
+
+}
