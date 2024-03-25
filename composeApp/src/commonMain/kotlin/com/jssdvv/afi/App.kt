766f588fbcb29f9da88a7c7fba98d749545f560d
jssdvv <jssdvv@gmail.com>
2024-03-25 00:30:45 -0500

    Renamed from Navigator to TabNavigation

--- composeApp/src/commonMain/kotlin/com/jssdvv/afi/App.kt
+++ composeApp/src/commonMain/kotlin/com/jssdvv/afi/App.kt
@@ -5,7 +5,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.jssdvv.afi.core.presentation.navigation.Navigator
+import com.jssdvv.afi.core.presentation.navigation.TabNavigation
 import com.jssdvv.afi.core.presentation.theme.Theme
 
 @Composable
@@ -15,8 +15,7 @@ fun App() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Navigator()
+            TabNavigation()
         }
     }
 }
-
---
+++ composeApp/src/commonMain/kotlin/com/jssdvv/afi/core/presentation/navigation/TabNavigation.kt
@@ -0,0 +1,58 @@
+package com.jssdvv.afi.core.presentation.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.navigator.tab.CurrentTab
+import cafe.adriel.voyager.navigator.tab.TabNavigator
+import com.jssdvv.afi.core.presentation.navigation.components.NavigationBar
+import com.jssdvv.afi.core.presentation.navigation.components.TopBar
+
+val LocalPaddingValues = compositionLocalOf { mutableStateOf(PaddingValues()) }
+val LocalNavigationBarShown = compositionLocalOf { mutableStateOf(true) }
+
+@Composable
+fun TabNavigation() {
+    val localPaddingValues = remember { mutableStateOf(PaddingValues()) }
+    val localNavigationBarShown = remember { mutableStateOf(true) }
+    val navigationTabs = listOf(
+        NavigationTabs.Scanner,
+        NavigationTabs.Formats,
+        NavigationTabs.Directory,
+        NavigationTabs.Maps
+    )
+    CompositionLocalProvider(
+        LocalPaddingValues provides localPaddingValues,
+        LocalNavigationBarShown provides localNavigationBarShown
+    ) {
+        TabNavigator(
+            tab = navigationTabs.first().tab,
+            disposeNestedNavigators = true
+        ) { tabNavigator ->
+            Scaffold(
+                modifier = Modifier,
+                bottomBar = {
+                    if (LocalNavigationBarShown.current.value) {
+                        NavigationBar(tabNavigator, navigationTabs)
+                    }
+                },
+                topBar = {
+                    when (tabNavigator.current.options.index.toUInt()) {
+                        0u -> TopBar()
+                        1u -> TopBar()
+                        2u -> TopBar()
+                        3u -> TopBar()
+                    }
+                }
+            ) { paddingValues ->
+                LocalPaddingValues.current.value = paddingValues
+                CurrentTab()
+            }
+        }
+    }
+}
