ba46bb9312c3dd0911191473f30afac2bd940b51
jssdvv <jssdvv@gmail.com>
2024-04-09 01:09:26 -0500

    Fix base Tabs

--- composeApp/src/commonMain/kotlin/com/jssdvv/afi/formats/presentation/screens/FormatsTab.kt
+++ composeApp/src/commonMain/kotlin/com/jssdvv/afi/formats/presentation/screens/FormatsTab.kt
@@ -1,39 +1,22 @@
 package com.jssdvv.afi.formats.presentation.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
-import com.jssdvv.afi.MR
-import com.jssdvv.afi.core.presentation.navigation.LocalContentPadding
-import dev.icerock.moko.resources.compose.stringResource
+import com.jssdvv.afi.core.presentation.navigation.NavigationItems
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.stringResource
 
 class FormatsTab : Tab {
-    override val options: TabOptions
-        @Composable
-        get() {
-            val title = stringResource(MR.strings.formats_tab_title)
-            return remember {
-                TabOptions(
-                    index = 1u,
-                    title = title,
-                    icon = null
-                )
-            }
-        }
+    @OptIn(ExperimentalResourceApi::class)
+    override val options
+        @Composable get() = TabOptions(
+            index = NavigationItems.FormatsItem.index,
+            title = stringResource(NavigationItems.FormatsItem.title)
+        )
 
     @Composable
     override fun Content() {
-        val paddingValues = LocalContentPadding.current
-        Column (
-            modifier = Modifier
-                .padding(paddingValues.value)
-        ) {
-            Text("Pantalla formatos")
-        }
+
     }
 }
