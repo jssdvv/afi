2430d6d3bd22d93fec8e909bff892c606b9429b2
jssdvv <jssdvv@gmail.com>
2024-11-07 00:55:15 -0500

    Delete old feature's implementations

--- composeApp/src/commonMain/kotlin/com/jssdvv/afi/directory/domain/repositories/HttpRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.jssdvv.afi.directory.domain.repositories
-
-import io.ktor.client.statement.HttpResponse
-
-interface HttpRepository {
-    suspend fun httpGetRequest(url: String): HttpResponse
-}
--- composeApp/src/commonMain/kotlin/com/jssdvv/afi/directory/domain/repositories/KsoupRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.jssdvv.afi.directory.domain.repositories
-
-interface KsoupRepository {
-    fun parseHtml(html: String)
-
-    fun parseXml(xml: String)
-}
